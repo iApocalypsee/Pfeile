03a57f1cc1bc7ca073cce69bcf6aa296b1efbb4c
Daniel Schmaus <daniel.schmaus@gmx.de>
2014-05-13 19:01:50 +0200

    AimSelectionScreen improved, but the ideas arn't implemented yet.

--- src/gui/AimSelectionScreen.java
+++ src/gui/AimSelectionScreen.java
@@ -9,18 +9,43 @@ import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import comp.Button;
+
 public class AimSelectionScreen extends Screen {
 
 	public static final String SCREEN_NAME = "AimSelection";
 	
 	public static final int SCREEN_INDEX = 4;
 	
+	private static World world = GameScreen.getInstance().getWorld();
+	
 	/** Background Color, if it need to be Transparent: 185/255 is black */
 	private static final Color TRANSPARENT_BACKGROUND = new Color(0, 0, 0, 185);
+
+	/** X-Position des Ausgewählten Feldes 
+	 * * (wenn noch nie auf <code> AimSelectionScreen </code> gecklickt wurde, ist der Wert -1)*/
+	private int posX_selectedField;
+	
+	/** Y-Position des Ausgewählten Feldes 
+	 * (wenn noch nie auf <code> AimSelectionScreen </code> gecklickt wurde, ist der Wert -1) */
+	private int posY_selectedField;
+	
+	private Button confirm = new Button (Main.getWindowWidth() - 300, Main.getWindowHeight() - 200, this, "Confirm");
+	
+	private Thread selectFieldThread; 
 	
 	
 	public AimSelectionScreen() {
 		super (SCREEN_NAME, SCREEN_INDEX);
+		
+		posX_selectedField = -1;
+		posY_selectedField = -1;
+		
+		getLastClickPosition();
+		
+//		selectFieldThread = new Thread (x);
+//		selectFieldThread.setDaemon(true);;
+//		x.run();
 	}
 
 	@Override 
@@ -40,23 +65,22 @@ public class AimSelectionScreen extends Screen {
 		GameScreen.getInstance().getWorld().drawReduced(g);
 	}
 	
-	private class MouseHandler implements MouseListener {
+	private class FieldSelector implements Runnable {
 
 		@Override
-		public void mouseClicked(MouseEvent arg0) {}
-
-		@Override
-		public void mouseEntered(MouseEvent arg0) {}
-
-		@Override
-		public void mouseExited(MouseEvent arg0) {}
-
-		@Override
-		public void mousePressed(MouseEvent arg0) {}
-
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO wenn auf das Feld gecklickt wurde muss es makiert werden
+		public void run() {
+			while (true) {
+				while (AimSelectionScreen.isRunningThread()) {
+					
+				}
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {e.printStackTrace();}
+			}
 		}
+	}
+	
+	private static boolean isRunningThread () {
+		return true;
 	}
 }
