bd1810a05a6f0813a7dcd3a46efac41efbbfb6af
Philip Schlösser <philipschloesser98@gmail.com>
2016-03-05 09:23:53 +0100

    Add simpler implementation of LangDict

--- src/general/langsupport/LangDict.scala
+++ src/general/langsupport/LangDict.scala
@@ -17,6 +17,37 @@ import scala.concurrent.{Await, Future}
 import scala.io.Source
 import scala.language.dynamics
 
+class LangDict() {
+  var data: JValue = null
+
+  def this(path: String) = {
+    this()
+    addJSON(path)
+  }
+
+  def addJSON(path: String): LangDict = {
+    val source = Source.fromFile("src/resources/data/language/" + path).getLines().mkString("\n");
+    data = parse(source).merge(data)
+    this
+  }
+
+  def translate(identifier: String, locale: String): String = {
+    val trans = compact(render(data \ identifier \ locale))
+    val eng = compact(render(data \ identifier \ "en_EN"))
+    if (trans != "")
+      trans
+    else if (eng != "")
+      eng
+    else
+      identifier
+  }
+
+  def tr(identifier: String): String = translate(identifier, Main.getLanguage.langCode)
+
+  @annotation.varargs
+  def tr(identifier: String, args: Object*) = String.format(tr(identifier), args:_*)
+}
+
 trait LanguageDictionary extends Dynamic {
 
   /**
