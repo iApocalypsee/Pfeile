79445b41fc17df2d8cd06582601c47893fd1eebc
Daniel Schmaus <daniel.schmaus@gmx.de>
2016-06-22 19:24:34 +0200

    Deprecated getTranslationIdentifier() from class Item Three names for one item, it really bad coding style, so I deprecated the unnessary getTranslationIdentifier() method and removed setName(String name), since that method disables the uniqueness of getName(). Later (the JSON-data has to change) the translation will work with Main.tr(getName()) instead of Main.tr(getTranslationIdentifier())

--- src/player/item/Item.java
+++ src/player/item/Item.java
@@ -17,21 +17,19 @@ public abstract class Item {
     /** String <code>name</code> is the unique coding name, like "WaterArrow" in JAVA style. Don't confuse it with the "displayed" names,
      * which are displayed somewhere on the screen to be seen by the user. This need should NOT be seen by the user. */
 	public Item(String codeName) {
-		if(codeName == null) throw new NullPointerException();
+		if(codeName == null)
+            throw new NullPointerException();
+
 		this.name = codeName;
 	}
 
-    /** That is the unique coding name, like "WaterArrow" in JAVA style. Don't confuse it with the "displayed" names,
+    /** That is the <b>unique coding name</b>, like "WaterArrow" in JAVA style. Don't confuse it with the "displayed" names,
      * which are displayed somewhere on the screen to be seen by the user. This need should NOT be seen by the user.
      *
      * @return the name for programming purposes
      */
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
     /**
@@ -44,7 +42,8 @@ public abstract class Item {
 
 
     /** Returns the name of this item in the language, it is given in <code>Main.getLanguage()</code>.
-     * this code is equal to <code>getNameDisplayed(Main.getLanguage())</code>
+     * this code is equal to <code>getNameDisplayed(Main.getLanguage())</code>. This name changes depending on the
+     * language and is therefore not unique.
      *
      * @return the name of this item for the user
      */
@@ -55,9 +54,22 @@ public abstract class Item {
     /**
      * Returns the translation identifier for this item.
      * @return The translation identifier for this item.
+     * @deprecated in a future commit, the method getTranslationIdentifier() will be removed. This method is completly
+     *             useless, since every item must already have an name. Consequently you can identify every item by it's
+     *             name, which must be unique. However, this property is already defined by the use as "name only for
+     *             programming purposes". <code>getNameDisplayed()</code> will <code>return Main.tr(name);</code> soon.
+     *             The JSON-classes must be changed though, but it's worth, because it's hard to handle three different
+     *             names.
      */
+    @Deprecated
     protected abstract String getTranslationIdentifier();
 
+    /**
+     * Use rather <code>getName()</code>, or <code>getNameDisplayed()</code> instead.
+     * This method right now is the same as <code>getName()</code>, but it is not recommended to use this method.
+     *
+     * @return the unique coding name. Not the name that is displayed.
+     */
     @Override
     public String toString () {
         return getName();
